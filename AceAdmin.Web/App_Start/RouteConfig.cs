700c12618ff71dfae3169b000e6927cea8fde6e8
Eason Wu <easonwu85@gmail.com>
2016-01-29 12:52:56 +0800

    1. Removed original HomeController and related views. 2. Merged ACE skin into Layout. 3. Added a new Blank page.

--- AceAdmin.Web/App_Start/RouteConfig.cs
+++ AceAdmin.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace AceAdmin.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "OtherPage", action = "Blank", id = UrlParameter.Optional }
             );
         }
     }
---
+++ AceAdmin.Web/Controllers/OtherPageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AceAdmin.Web.Controllers
+{
+    public class OtherPageController : BaseController
+    {
+        // GET: OtherPage
+        public ActionResult Blank()
+        {
+            return View();
+        }
+    }
+}
